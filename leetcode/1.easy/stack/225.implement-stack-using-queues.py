21461699b95521ec9bf43d09973eba6b5a137598
khoaguin <dkn.work@protonmail.com>
2023-12-08 10:15:44 +0700

    done implementing a stack using one queue

--- leetcode/1.easy/stack/225.implement-stack-using-queues.py
+++ leetcode/1.easy/stack/225.implement-stack-using-queues.py
@@ -6,25 +6,75 @@
 
 
 # @lc code=start
+
+import collections
+
+
 class MyStack:
+    """
+    Simplest implementation using a list
+        Time Complexity: append and pop operations from both the ends
+          using a python List provides O(n) time complexity.
+
+    Result:
+        Runtime beats 19.21% of python3 submissions (41 ms)
+        Mem usage beats 51.95% of python3 submissions (16.3 MB)
+    """
+
     def __init__(self):
-        self.data = []
+        self._data = []
 
     def push(self, x: int) -> None:
         # Pushes element x to the top of the stack.
-        self.data.append(x)
+        self._data.append(x)
 
     def pop(self) -> int:
         # Removes the element on the top of the stack and returns it.
-        return self.data.pop()
+        return self._data.pop()
 
     def top(self) -> int:
         # Returns the element on the top of the stack.
-        return self.data[-1] if len(self.data) > 0 else None
+        return self._data[-1] if len(self._data) > 0 else None
 
     def empty(self) -> bool:
         # Returns true if the stack is empty, false otherwise.
-        return not bool(len(self.data))
+        return not bool(len(self._data))
+
+
+class MyStack:
+    """
+    Implementation using one dequeue (Doubly Ended Queue)
+        Time Complexity: append and pop operations from both the ends
+          using a python collections.deque() provides O(1) time complexity.
+
+    Note: Must use only standard operations of a queue, which means that only
+        push to back (.append), peek/pop from front (index 0), size (len)
+        and is empty operations are valid.
+
+    Result:
+        Runtime beats 34.01% of python3 submissions (41 ms)
+        Mem usage beats 51.95% of python3 submissions (16.3 MB)
+    """
+
+    def __init__(self):
+        self._data = collections.deque()
+
+    def push(self, x: int) -> None:
+        # Pushes element x to the top of the stack.
+        self._data.append(x)  # append = push to back
+        self._data.rotate(1)
+
+    def pop(self) -> int:
+        # Removes the element on the top of the stack and returns it.
+        return self._data.popleft()
+
+    def top(self) -> int:
+        # Returns the element on the top of the stack.
+        return self._data[0] if len(self._data) > 0 else None
+
+    def empty(self) -> bool:
+        # Returns true if the stack is empty, false otherwise.
+        return not len(self._data)
 
 
 if __name__ == "__main__":
@@ -32,7 +82,7 @@ if __name__ == "__main__":
     myStack = MyStack()
     myStack.push(1)
     myStack.push(2)
-    print(myStack.data)
+    print(myStack._data)
     print(myStack.top())  # return 2
     print(myStack.pop())  # return 2
     print(myStack.empty())  # return False
