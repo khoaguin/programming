5f0b38866853d1bab45ad33a2f2d0b20d3ecf953
khoaguin <dkn.work@protonmail.com>
2023-12-17 10:18:29 +0700

    done leetcode "104.maximum-depth-of-binary-tree"

---
+++ leetcode/1.easy/tree/104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        """
+        My solution
+            Run time: O(n) - we visit each node once. Beats 57.66% of users with Python3
+            Space: avg case O(log n). Worst case O(n) when the tree is linear.
+                    Beats 8.42% of users with Python3
+        """
+
+        def find_max_depth(root: Optional[TreeNode], max_depth: int) -> int:
+            if not root:
+                # if a node is None, it means that we reached the terminate node.
+                # return the max_depth found
+                return max_depth
+            else:
+                # if a node is not None, recursively find the maximum depth of its children nodes
+                max_depth = max(
+                    find_max_depth(root.left, max_depth + 1),
+                    find_max_depth(root.right, max_depth + 1),
+                )
+                return max_depth
+
+        return find_max_depth(root, 0)
+
+
+# @lc code=end
