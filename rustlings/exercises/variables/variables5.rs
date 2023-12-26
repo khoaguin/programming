3498dd054d62b74be082cad9daae525a4a2bffc4
khoaguin <dkn.work@protonmail.com>
2023-12-26 19:35:55 +0700

    learning Rust - reading chapter 4. Doing rustlings exercises

--- rustlings/exercises/variables/variables5.rs
+++ rustlings/exercises/variables/variables5.rs
@@ -4,6 +4,10 @@
 // hint.
 
 fn main() {
+    // As you saw in the guessing game tutorial in Chapter 2, you can declare a new
+    // variable with the same name as a previous variable. Rustaceans say that the
+    // first variable is shadowed by the second, which means that the second variable
+    // is what the compiler will see when you use the name of the variable.
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
     let number: i32 = 3; // don't rename this variable
