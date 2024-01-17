94babde4056f4ea0656af84778e85db1ce3c6637
khoaguin <dkn.work@protonmail.com>
2024-01-17 09:56:35 +0700

    done array: can-place-flowers?, doing rust book chap 11

--- leetcode/1.easy/array/605.can-place-flowers.py
+++ leetcode/1.easy/array/605.can-place-flowers.py
@@ -11,32 +11,47 @@ from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         """
-        Move through the flowerbed and plant the flower on the way
+        Greedy: Move through the flowerbed and plant the flower on the way
         If we run out of plots, return False. Otherwise return True
         """
-        i = 0
-        if len(flowerbed) == 1:
-            return not n
-        while i < len(flowerbed):
-            if flowerbed[i] == 1:
-                i += 2
-            else:
-                if flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
-                    flowerbed[i] = 1
-                    n = n - 1
-                    i += 2
+        if n == 0:
+            return True
+        num_plots = len(flowerbed)
+        if num_plots == 1:
+            return True if flowerbed[0] == 0 else False
+        for i in range(num_plots):
+            if (
+                (i == 0 and flowerbed[i + 1] == 0 and flowerbed[i] == 0)
+                or (
+                    0 < i < num_plots - 1
+                    and flowerbed[i] == 0
+                    and flowerbed[i - 1] == 0
+                    and flowerbed[i + 1] == 0
+                )
+                or (i == num_plots - 1 and flowerbed[i - 1] == 0 and flowerbed[i] == 0)
+            ):
+                flowerbed[i] = 1
+                n = n - 1
 
-        return False if n > 0 else True
-        # if i == 0 and :
+                # Skip the next plot as it can't be used now
+                if i < num_plots - 1:
+                    i += 1
 
-        # if i == len(flowerbed) - 1:
+            # Early exit if all required flowers are planted
+            if n <= 0:
+                return True
+
+        # print(n)
+        # print(flowerbed)
+        return n <= 0
 
 
-# s = Solution()
-# print(s.canPlaceFlowers(flowerbed=[1, 0, 0, 0, 1], n=2))
-# print(s.canPlaceFlowers(flowerbed=[0, 1], n=1))
-# print(s.canPlaceFlowers(flowerbed=[0], n=1))
-# print(s.canPlaceFlowers(flowerbed=[0, 1, 0, 1], n=1))
-# print(s.canPlaceFlowers(flowerbed=[1], n=1))
+s = Solution()
+print(s.canPlaceFlowers(flowerbed=[1, 0, 0, 0, 1], n=1))  # True
+print(s.canPlaceFlowers(flowerbed=[0, 1], n=1))  # False
+print(s.canPlaceFlowers(flowerbed=[0], n=1))  # True
+print(s.canPlaceFlowers(flowerbed=[0, 1, 0, 1], n=1))  # False
+print(s.canPlaceFlowers(flowerbed=[1], n=1))  # False
+print(s.canPlaceFlowers(flowerbed=[0, 0, 1, 0, 0], n=1))  # True
 
 # @lc code=end
