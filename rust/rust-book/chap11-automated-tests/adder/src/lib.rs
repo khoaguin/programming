9829ef0fd0a12d29e024426c199138eb47642ab4
khoaguin <dkn.work@protonmail.com>
2024-02-19 08:20:54 +0700

    done chapter11 of the rust book

--- rust/rust-book/chap11-automated-tests/adder/src/lib.rs
+++ rust/rust-book/chap11-automated-tests/adder/src/lib.rs
@@ -1,5 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -7,25 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
-        // assert_eq!(2 + 2, 5);
-    }
-
-    #[test]
-    #[should_panic(expected = "less than or equal to 100")]
-    fn greater_than_100() {
-        assert!(200 < 100);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
