ad46a70587def85f4fbbd524a461787c68db7919
khoaguin <dkn.work@protonmail.com>
2024-05-28 08:37:58 +0700

    [rust] sending job to workers via a channel

--- rust/rust-book/chap20-multithreaded-web-server/hello/src/lib.rs
+++ rust/rust-book/chap20-multithreaded-web-server/hello/src/lib.rs
@@ -1,11 +1,14 @@
-use std::{sync::mpsc, thread};
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
-    workers: Vec<Worker>,  // hold on to the receiver
-    sender: mpsc::Sender<Job>,  // create a channel and hold on to the sender
+    workers: Vec<Worker>,      // hold on to the receiver
+    sender: mpsc::Sender<Job>, // create a channel and hold on to the sender
 }
 
-struct Job;  // hold the closures we want to send down the channel
+struct Job; // hold the closures we want to send down the channel
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
@@ -13,14 +16,17 @@ impl ThreadPool {
 
         let (sender, receiver) = mpsc::channel();
 
+        // The `Arc` type will let multiple workers own the receiver
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             // create some threads and store them in the vector
-            workers.push(Worker::new(id, receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers , sender}
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -29,7 +35,10 @@ impl ThreadPool {
                                       // one thread to another and 'static because
                                       // we don’t know how long the thread will take to execute.
     {
+        let job = Box::new(f);
+
         // send the job it wants to execute through the sender
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -39,11 +48,19 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // we need the closure to loop forever, asking the receiving end of
+        // the channel for a job and running the job when it gets one.
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock() //  we first call lock on the receiver to acquire the mutex
+                .unwrap() // then we call unwrap to panic on any errors
+                .recv() // If we get the lock on the mutex, we call recv to receive a Job from the channel
+                .unwrap();
+            println!("Worker {id} got a job; executing.");
+            job();
         });
 
-        Worker {id, thread}
+        Worker { id, thread }
     }
 }
